d09c6a0aa71ca84480d1743d8f960f21c5c9e830
DragonMo <975425198@qq.com>
2022-07-06 11:15:41 +0800

    ModBusRtu2MQTT

--- src/handle/SerialHandle.go
+++ src/handle/SerialHandle.go
@@ -5,8 +5,8 @@ import (
 	log "modbusRtu2Mqtt/src/userlog"
 )
 
-func Serial(msg chan []byte) {
-	for bytes := range msg {
+func Serial(msg *[]byte) {
+	for bytes := range *msg {
 		sprintf := fmt.Sprintf("%x", bytes)
 		log.Info("receive serial data:：", sprintf)
 	}
